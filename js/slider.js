c33d2e16803a21bd976f9fc51f341ed9fef0ab6b
Slaviya <sergeykiloshenko@list.ru>
2022-09-17 00:47:52 +0300

    test slider

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,25 @@
+const brandList = document.querySelectorAll('.brend-item');
+const itemWrapper = document.querySelector('.item__wrapper');
+const crs = document.querySelectorAll('.crs');
+const list = document.querySelector('.by-brend__list');
+const Slider = (el) => {
 
-const swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clicable: true,
-    },
-});
+    for (let i = 0; i < el.length; i++) {
+        let crs = document.createElement('img');
+        if (i == 0) {
+            crs.className = `crs crs${i} crs--active`;
+        } else {
+            crs.className = `crs crs${i}`;
+        }
+        itemWrapper.append(crs);
+    }
+}
+Slider(brandList);
+
+itemWrapper.addEventListener('click', e => {
+    let crs = document.querySelectorAll('.crs');
+    let ctn = e.target.classList[0];
+    list.style.transform = 'translate(-250px)';
+})
+
+
